283f520ab2d48a835777b9b21665ffdee6968496
Ashiya Kandhway <ashiyakandhway27july@gmail.com>
2023-05-07 13:39:00 +0530

    sign up student

--- lib/Screens/SignUp/signup_form.dart
+++ lib/Screens/SignUp/signup_form.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import '../Home_Club.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -14,9 +13,9 @@ class SignUpFormWidget extends StatefulWidget {
 class _SignUpFormWidgetState extends State<SignUpFormWidget> {
   TextEditingController _emailtextcontroller = TextEditingController();
   TextEditingController _passwordtextcontroller = TextEditingController();
-  TextEditingController _phoneno = TextEditingController();
+
   TextEditingController _clubname = TextEditingController();
-  TextEditingController _fullname = TextEditingController();
+
 
   bool pos=true;
   bool error=false;
@@ -28,25 +27,11 @@ class _SignUpFormWidgetState extends State<SignUpFormWidget> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextFormField(
-              controller: _fullname,
-              decoration: const InputDecoration(
-                  label: Text("Full Name"),
-                  prefixIcon: Icon(Icons.person_outline_rounded)),
-            ),
-            const SizedBox(height: 5),
-            TextFormField(
               controller: _emailtextcontroller,
               keyboardType: TextInputType.emailAddress,
               decoration: const InputDecoration(
                   label: Text("Email id"),
                   prefixIcon: Icon(Icons.email_outlined)),
-            ),
-            const SizedBox(height: 5),
-            TextFormField(
-              controller: _phoneno,
-              decoration: const InputDecoration(
-
-                  label: Text("Phone Number"), prefixIcon: Icon(Icons.numbers)),
             ),
             const SizedBox(height: 5),
             TextFormField(
@@ -68,7 +53,7 @@ class _SignUpFormWidgetState extends State<SignUpFormWidget> {
               child: ElevatedButton(
                 onPressed: () async {
                   print("submit Button Pressed");
-                  if(_clubname.text.isEmpty || _fullname.text.isEmpty || _emailtextcontroller.text.isEmpty || _phoneno.text.isEmpty
+                  if(_clubname.text.isEmpty || _emailtextcontroller.text.isEmpty
                   || _passwordtextcontroller.text.isEmpty){
                     pos=false;
                   }else pos= true;
@@ -94,10 +79,8 @@ class _SignUpFormWidgetState extends State<SignUpFormWidget> {
                         .collection('clubs')
                         .doc(FirebaseAuth.instance.currentUser?.uid)
                         .update({
-                    "Admin_name": _fullname.text,
                     "Name": _clubname.text,
                     "Email": _emailtextcontroller.text,
-                    "Contact": int.parse(_phoneno.text),
                     }).then((value) {
                     print("Club data added");
                         });
--- lib/Screens/SignUp/signup_form_student.dart
+++ lib/Screens/SignUp/signup_form_student.dart
@@ -1,11 +1,23 @@
 import 'package:club_management/Screens/Home_Student.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class SignUpFormStudentWidget extends StatelessWidget {
-  const SignUpFormStudentWidget({
-    Key? key,
-  }) : super(key: key);
 
+class SignUpFormStudentWidget extends StatefulWidget {
+  const SignUpFormStudentWidget({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpFormStudentWidget> createState() => _SignUpFormStudentWidgetState();
+}
+
+class _SignUpFormStudentWidgetState extends State<SignUpFormStudentWidget> {
+  TextEditingController _emailtextcontroller = TextEditingController();
+  TextEditingController _passwordtextcontroller = TextEditingController();
+  TextEditingController _fullname = TextEditingController();
+  TextEditingController _contact = TextEditingController();
+  bool pos=true;
+  bool error=false;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -15,23 +27,30 @@ class SignUpFormStudentWidget extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextFormField(
+              controller: _fullname,
               decoration: const InputDecoration(
                   label: Text("Full Name"),
                   prefixIcon: Icon(Icons.person_outline_rounded)),
             ),
             const SizedBox(height: 5),
             TextFormField(
+              controller: _emailtextcontroller,
+              keyboardType: TextInputType.emailAddress,
               decoration: const InputDecoration(
                   label: Text("Email id"),
                   prefixIcon: Icon(Icons.email_outlined)),
             ),
             const SizedBox(height: 5),
             TextFormField(
+              controller: _contact,
+              keyboardType: TextInputType.phone,
               decoration: const InputDecoration(
-                  label: Text("Phone Numer"), prefixIcon: Icon(Icons.numbers)),
+                  label: Text("Phone Number"), prefixIcon: Icon(Icons.numbers)),
             ),
             const SizedBox(height: 5),
             TextFormField(
+              controller: _passwordtextcontroller,
+              obscureText: true,
               decoration: const InputDecoration(
                   label: Text("Password"), prefixIcon: Icon(Icons.fingerprint)),
             ),
@@ -39,7 +58,40 @@ class SignUpFormStudentWidget extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
+                  print("submit Button Pressed");
+                  if(_fullname.text.isEmpty || _emailtextcontroller.text.isEmpty || _contact.text.isEmpty
+                      || _passwordtextcontroller.text.isEmpty){
+                    pos=false;
+                  }else pos= true;
+                  if (pos){
+                    setState(() {
+                      error=false;
+                    });
+                  }
+                  FirebaseAuth.instance.createUserWithEmailAndPassword(email: _emailtextcontroller.text,
+                      password:_passwordtextcontroller.text ).then((value) {
+                    print('Created new account');
+                    FirebaseFirestore.instance.collection('students')
+                        .doc(FirebaseAuth.instance.currentUser?.uid)
+                        .set({})
+                        .then((value) => print(
+                        'Created user ${FirebaseAuth.instance.currentUser?.uid}'))
+                        .catchError((error)=>print(error));
+                    print("Created new account again");
+                  }).onError((error, stackTrace) {
+                    print("Error ${error.toString()}");
+                  });
+                  await FirebaseFirestore.instance
+                      .collection('students')
+                      .doc(FirebaseAuth.instance.currentUser?.uid)
+                      .update({
+                    "Name": _fullname.text,
+                    "Email": _emailtextcontroller.text,
+                    "Contact_no": _contact.text,
+                  }).then((value) {
+                    print("Student data added");
+                  });
                   Navigator.push(context,
                       MaterialPageRoute(builder: (context) => HomeStudent()));
                 },
